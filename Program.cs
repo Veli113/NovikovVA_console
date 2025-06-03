692bb6d9db46d48ef33a3573c0a88cbc45cc7ca4
NovikovVA <verloni2002@mail.ru>
2025-06-03 18:03:58 +0300

    Add *.cs-files

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovikovVA
+{
+    internal class Program
+    {
+        static public double Sqr(double X)
+        {
+            return X * X;
+        }
+    }
+}
+//NovikovVA Am-1-24
